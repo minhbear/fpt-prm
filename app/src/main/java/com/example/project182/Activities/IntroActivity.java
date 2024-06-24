91cb15182cbaa294ebed8b9dd94211e1cfeb1a15
minhbear <minhbeardev@gmail.com>
2024-06-25 00:24:59 +0700

    feat: init

---
+++ app/src/main/java/com/example/project182/Activities/IntroActivity.java
@@ -0,0 +1,27 @@
+package com.example.project182.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project182.databinding.ActivityIntroBinding;
+
+
+public class IntroActivity extends AppCompatActivity {
+    ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.startBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+
+        Window w=getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
